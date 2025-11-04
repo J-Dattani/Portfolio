cb272bddfa8e69c77f740108a4954bbd8d261169
Jaymin Dattani <jaymin.dattani141327@marwadiuniversity.ac.in>
2025-11-04 23:44:48 +0530

    `Added new achievement to achievements.js file with patent publication details.`

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -1,4 +1,5 @@
 import pf from '../assets/docs/𝗣𝗿𝗼𝗷𝗲𝗰𝘁 𝗙𝗮𝗶𝗿 𝟮𝟬𝟮𝟱.jpeg'
+import sc from '../assets/docs/patent.jpeg'
 
 // Add your achievements here. `description` is optional.
 export const achievements = [
@@ -10,5 +11,14 @@ export const achievements = [
     image: pf,
     description: 'The event showcased a wide range of student projects, each demonstrating practical thinking, creativity, and technical skills. It was a great opportunity to share our work, gain insights and engage with other students and faculty members.Our project, 𝗦𝗲𝗮𝗿𝗰𝗵𝟰𝗖𝗮𝗿𝗲𝗲𝗿, was selected for the fair. It is a Website designed to help students find the most suitable schools and colleges based on their academic performance, interests, and preferences. The platform offers personalized search options, secure login with OTP verification, and dedicated dashboards for both students and administrators—making it easier to navigate academic choices with confidence.',
   },
+    {
+    id: 'Patent Published',
+    title: '𝗦𝗲𝗮𝗿𝗰𝗵𝟰𝗖𝗮𝗿𝗲𝗲𝗿 - Patent Published',
+    issuer: 'Marwadi University',
+    year: '2025',
+    image: sc,
+    description: 'Patent for our final year project, 𝗦𝗲𝗮𝗿𝗰𝗵𝟰𝗖𝗮𝗿𝗲𝗲𝗿—𝗮 𝗰𝗮𝗿𝗲𝗲𝗿 𝗴𝘂𝗶𝗱𝗮𝗻𝗰𝗲 𝘀𝘆𝘀𝘁𝗲𝗺—has been officially 𝗽𝘂𝗯𝗹𝗶𝘀𝗵𝗲𝗱 .This achievement is made even more special by the official 𝗟𝗲𝘁𝘁𝗲𝗿 𝗼𝗳 𝗔𝗽𝗽𝗿𝗲𝗰𝗶𝗮𝘁𝗶𝗼𝗻 we received for this work.',
+  },
+  
 
 ]
